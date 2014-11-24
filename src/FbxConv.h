e436d8e60107a88baa9683aec3f870f1fdf4454f
lvlong <lvlong@chukong-inc.com>
2014-11-24 09:15:55 +0800

    modify version to rc1

--- src/FbxConv.h
+++ src/FbxConv.h
@@ -26,9 +26,9 @@
 
 #ifndef BUILD_ID
 #ifdef DEBUG
-#define BUILD_ID "debug version for v3.3 final"
+#define BUILD_ID "debug version for v3.3 rc1"
 #else
-#define BUILD_ID "pre-release for v3.3 final"
+#define BUILD_ID "pre-release for v3.3 rc1"
 #endif
 #endif
 
