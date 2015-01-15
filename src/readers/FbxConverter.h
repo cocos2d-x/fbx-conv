c163dfc5798defd06d51cc57a057ef4731bb3c6f
lvlong <lvlong@chukong-inc.com>
2015-01-15 14:37:26 +0800

    fix bug, calculate length

--- src/readers/FbxConverter.h
+++ src/readers/FbxConverter.h
@@ -922,7 +922,7 @@ namespace readers {
                 if(frames.size() == 0)
                     continue;
 
-                float length = animStop;
+                float length = frames[frames.size()-1]->time;
                 animation->length = length / 1000.f;
 
 				// Only add keyframes really needed
